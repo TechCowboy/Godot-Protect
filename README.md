b995d84261ae3dbbb80c63603d5519be71830212
Norman Davie <ecompservices@gmail.com>
2026-09-11 13:00:57 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 Simple steps to protect your Godot project.
 This simply stops script kiddies, it does not protect against determined pirates.
+
+Prerequistes:
+    git                     sudo apt install git
+    github CLI              see https://cli.github.com/
+    scons build system      sudo apt install scons
+    Godot-Secure            gh repo clone KnifeXRage/Godot-Secure
+    Godot Source            gh repo clone godotengine/godot -- -b 4.7
+    Screen Reader Support   python godot/misc/scripts/install_accesskit.py
+    
+    
+
+Set up your directory structure like this:
+   eg.  Projects --- Godot-Protect
+                  |--my_godot_project
+                  |- Godot-Secure
+                  |--godot  
+       
+
+        
+How to run:
+    cd my_godot_project
+    ..\Godot-Project\protech.sh
+    
+    
+    
+    
+
--- protect.sh
+++ protect.sh
@@ -1,4 +1,37 @@
-echo Ensure you have "Encrypt Exported PCK" and "Encrypt Index" in a custom template
+echo This will take about 45 minutes to complete
+sleep 3
+
+set -x
+
+# get randomized encryption key and store in project
 openssl rand -hex 32 > godot.gdkey
+
+# use that key when building godot
 export SCRIPT_AES256_ENCRYPTION_KEY=$(cat godot.gdkey)
+
+cd ..
+
+# remove the existing godot project if it exists
+rm godot -r -f
+
+# get the latest 4.7 branch
+gh repo clone godotengine/godot -- -b 4.7
+
+# modify the fresh godot sources so they are secure
+python ../Godot-Secure/godot_secure.py godot/
+
+# build the linux project
+scons platform=linuxbsd target=editor use_mingw=yes 
+
+scons platform=windows target=template_debug use_mingw=yes
+scons platform=windows target=template_release use_mingw=yes
+
+scons platform=linux target=template_debug use_mingw=yes
+scons platform=linux target=template_release use_mingw=yes
+
+set +x
+
+# Execute the modified editor
+bin/godot.linuxbsd.editor.x86_64
+
 
